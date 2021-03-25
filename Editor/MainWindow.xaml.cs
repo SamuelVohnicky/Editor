ea6ebfdbebedb06c155e5b6ec5321215b398b684
SamuelVohnicky <43740161+SamuelVohnicky@users.noreply.github.com>
2021-03-25 23:44:53 +0100

    fsgda

--- Editor/MainWindow.xaml.cs
+++ Editor/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +26,10 @@ namespace Editor
         {
             InitializeComponent();
         }
+        int[,] forto;
+        WriteableBitmap fonda;
 
+        private BitmapImage fotka;
         private void button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog op = new OpenFileDialog();
@@ -36,7 +40,119 @@ namespace Editor
             if (op.ShowDialog() == true)
             {
                 imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+                fotka = new BitmapImage(new Uri(op.FileName));
+                
             }
         }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            forto = Array2DBMIConverter.BitmapImageToArray2D(fotka);
+            for (int x = 0; x < forto.GetLength(0); x++)
+            {
+                for (int y = 0; y <  forto.GetLength(1); y++)
+                {
+                    forto[x, y] -= 10;
+                }
+            }
+            fonda = Array2DBMIConverter.Array2DToWriteableBitmap(forto, fotka);
+            imgPhoto.Source = Array2DBMIConverter.ConvertWriteableBitmapToBitmapImage(fonda);
+            fotka = Array2DBMIConverter.ConvertWriteableBitmapToBitmapImage(fonda);
+        }
+    }
+
+    static class Array2DBMIConverter
+    {
+        public static int[,] BitmapImageToArray2D(BitmapImage src)
+        {
+            int[,] array2D = new int[src.PixelHeight, src.PixelWidth];
+
+            WriteableBitmap wb = new WriteableBitmap(src);
+            int width = wb.PixelWidth;
+            int height = wb.PixelHeight;
+            int bytesPerPixel = (wb.Format.BitsPerPixel + 7) / 8;
+            int stride = wb.BackBufferStride;
+            wb.Lock();
+            unsafe
+            {
+                byte* pImgData = (byte*)wb.BackBuffer;
+                int cRowStart = 0;
+                int cColStart = 0;
+                for (int row = 0; row < height; row++)
+                {
+                    cColStart = cRowStart;
+                    for (int col = 0; col < width; col++)
+                    {
+                        byte* bPixel = pImgData + cColStart;
+                        //bPixel[0] // Blue
+                        //bPixel[1] // Green
+                        //bPixel[2] // Red
+                        int pixel = bPixel[2]; //Red
+                        pixel = (pixel << 8) + bPixel[1]; //Green
+                        pixel = (pixel << 8) + bPixel[0]; //Blue
+                        array2D[row, col] = pixel;
+
+                        cColStart += bytesPerPixel;
+                    }
+                    cRowStart += stride;
+                }
+            }
+            wb.Unlock();
+            wb.Freeze();
+
+            return array2D;
+        }
+        public static WriteableBitmap Array2DToWriteableBitmap(int[,] array2D, BitmapImage src)
+        {
+            WriteableBitmap wb = new WriteableBitmap(src);
+            int width = wb.PixelWidth;
+            int height = wb.PixelHeight;
+            int bytesPerPixel = (wb.Format.BitsPerPixel + 7) / 8;
+            int stride = wb.BackBufferStride;
+            wb.Lock();
+            unsafe
+            {
+                byte* pImgData = (byte*)wb.BackBuffer;
+                int cRowStart = 0;
+                int cColStart = 0;
+                for (int row = 0; row < height; row++)
+                {
+                    cColStart = cRowStart;
+                    for (int col = 0; col < width; col++)
+                    {
+                        byte* bPixel = pImgData + cColStart;
+
+                        bPixel[0] = (byte)((array2D[row, col] & 0xFF));// Blue
+                        bPixel[1] = (byte)((array2D[row, col] & 0xFF00) >> 8);// Green
+                        bPixel[2] = (byte)((array2D[row, col] & 0xFF0000) >> 16);// Red
+
+                        cColStart += bytesPerPixel;
+                    }
+                    cRowStart += stride;
+                }
+            }
+            wb.Unlock();
+            wb.Freeze();
+
+            return wb;
+        }
+
+        public static BitmapImage ConvertWriteableBitmapToBitmapImage(WriteableBitmap wbm)
+        {
+            BitmapImage bmImage = new BitmapImage();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(wbm));
+                encoder.Save(stream);
+                bmImage.BeginInit();
+                bmImage.CacheOption = BitmapCacheOption.OnLoad;
+                bmImage.StreamSource = stream;
+                bmImage.EndInit();
+                bmImage.Freeze();
+            }
+            return bmImage;
+        }
+
     }
 }
